023bf483833508e22deb819161f81f3d36b92d01
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-10-18 23:22:54 -0400

    Intento de implementacion de la clase y del metodo de euler
    no funciona :s

--- planeta.py
+++ planeta.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+G=1
+M=1
+m=1
+
 class Planeta(object):
     '''
     Complete el docstring.
@@ -12,7 +17,7 @@ class Planeta(object):
         instancias de una clase.
 
         Ej. de uso:
-        >> mercurio = Planeta([x0, y0, vx0, vy0])
+        >> Aiur = Planeta([x0, y0, vx0, vy0])
         >> print(mercurio.alpha)
         >> 0.
         '''
@@ -20,15 +25,26 @@ class Planeta(object):
         self.t_actual = 0.
         self.alpha = alpha
 
-    def ecuacion_de_movimiento(self):
+    def ecuacion_de_movimiento(self,dr=np.array([0,0,0,0])):
         '''
         Implementa la ecuación de movimiento, como sistema de ecuaciónes de
         primer orden.
         '''
+        G=1
+        M=1
         x, y, vx, vy = self.y_actual
-        # fx = ...
-        # fy = ...
-        return [vx, vy, fx, fy]
+        dx=dr[0]
+        dy=dr[1]
+        dvx=dr[2]
+        dvy=dr[3]
+        x=x+dx
+        y=y+dy
+        vx=vx+dvx
+        vy=vy+dvy
+        R=np.sqrt(x**2+y**2)
+        ax=x*(G*M)*(((2.*self.alpha)/(R**4.))-(1./(R**3.)))
+        ay=y*(G*M)*((2.*self.alpha)/(R**4.)-1./(R**3.))
+        return np.array([vx, vy, ax, ay])
 
     def avanza_euler(self, dt):
         '''
@@ -36,24 +52,50 @@ class Planeta(object):
         en un intervalo de tiempo dt usando el método de Euler explícito. El
         método no retorna nada, pero re-setea los valores de self.y_actual.
         '''
-        pass
+        yn=self.y_actual + dt*(self.ecuacion_de_movimiento())
+        self.y_actual=yn
+        self.t_actual+=dt
+#        pass
 
     def avanza_rk4(self, dt):
         '''
         Similar a avanza_euler, pero usando Runge-Kutta 4.
         '''
-        pass
+        k1=dt*self.ecuacion_de_movimiento()
+        k2=dt*self.ecuacion_de_movimiento(k1/2.)
+        k3=dt*self.ecuacion_de_movimiento(k2/2.)
+        k4=dt*self.ecuacion_de_movimiento(k3)
+        yn=self.y_actual+(k1+2*k2+2*k3+k4)/6.
+        self.y_actual=yn
+        self.t_actual=self.t_actual+dt
+#        pass
 
     def avanza_verlet(self, dt):
         '''
         Similar a avanza_euler, pero usando Verlet.
         '''
-        pass
+        r=self.y_actual[0],self.y_actual[1]
+        v=self.y_actual[2],self.y_actual[3]
+        a=self.ecuacion_de_movimiento[2],self.ecuacion_de_movimiento[3]
+        rn=r+dt*v+dt**2*a/2.
+        dr=rn-r
+        ec_de_mov_dt=self.ecuacion_de_movimiento([dr[0],dr[1],0,0])
+        a2=ec_de_mov_dt[2],ec_de_mov_dt[3]
+        vn=v+(a+a2)*dt/2.
+        self.y_actual=rn[0],rn[1],vn[0],vn[1] #poner como np.array?
+#        pass
 
     def energia_total(self):
         '''
         Calcula la enérgía total del sistema en las condiciones actuales.
         '''
-        pass
-
-
+        G=1
+        M=1
+        m=1
+        r=self.y_actual[0],self.y_actual[1]
+        v=self.y_actual[2],self.y_actual[3]
+        R=np.sqrt(r[0]**2+r[1]**2)
+        T=m*(v[0]**2+v[1]**2)/2.
+        V=(G*M*m)*(self.alpha/R**2 - 1./R)
+        return T+V
+#        pass
--- solucion_usando_euler.py
+++ solucion_usando_euler.py
@@ -2,12 +2,39 @@
 # -*- coding: utf-8 -*-
 
 from planeta import Planeta
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig=plt.figure()
+fig.clf()
+
+condicion_inicial = [10, 0, 0, 0.4]
+
+Aiur = Planeta(condicion_inicial)
+N=3*np.int(1e4)
+tf=5000
+dt=tf/N
+t=np.linspace(0,tf,N)
+x=np.zeros(N)
+y=np.zeros(N)
+E=np.zeros(N)
+
+x[0]=condicion_inicial[0]
+y[0]=condicion_inicial[1]
+E[0]=Aiur.energia_total()
+
+for n in range(1,N):
+    Aiur.avanza_euler(dt)
+    x[n]=Aiur.y_actual[0]
+    y[n]=Aiur.y_actual[1]
+    E[n]=Aiur.energia_total()
+print t
+print x
 
 
-condicion_inicial = [10, 0, 0, XXX]
-
-p = Planeta(condicion_inicial)
-
-'''
-Complete el código a continuación.
-'''
+ax1=fig.add_subplot(211)
+ax1.plot(x,y)
+ax2=fig.add_subplot(212)
+ax2.plot(t,E)
+plt.draw()
+plt.show()
