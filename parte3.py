64a84172de4db4a77566d31f7ac12231f5029459
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-10-20 22:39:48 -0400

    Se implementa la parte 3
    Se obtiene el calculo de la velocidad angular, al fin.

--- parte3.py
+++ parte3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import division
 from planeta import Planeta
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,21 +14,86 @@ alpha = 10**(-2.622) #RUT: 18.668.622-5
 
 Shakuras = Planeta(condicion_inicial, alpha)
 N=3*np.int(1e4)
-dt=7000./N
-t=np.linspace(0,7000,N)
+tf=3690. #tf=124 da una vuelta
+dt=tf/N
+t=np.linspace(0,tf,N)
 x=np.zeros(N)
 y=np.zeros(N)
 E=np.zeros(N)
+r= np.zeros(N)
+#phi = np.zeros(N)
+#p = [0]
 
 x[0]=condicion_inicial[0]
 y[0]=condicion_inicial[1]
 E[0]=Shakuras.energia_total()
+r[0] = x[0]** 2 + y[0]** 2
+#n=1
+#phi[0] = np.arctan(y[0]/ x[0])
 
-for n in range(1,N):
+'''
+for n in range (1,590) :
     Shakuras.avanza_verlet(dt)
     x[n]=Shakuras.y_actual[0]
     y[n]=Shakuras.y_actual[1]
     E[n]=Shakuras.energia_total()
+    r[n] = x[n]** 2 + y[n]** 2
+
+
+
+    if x[n] != 0 and x[n-1] != 0:
+        a = y[n]/ x[n]
+        phi[n] = np.arctan(a)
+#    else:
+#        phi[]
+    if phi[n] < phi[n-1]:
+            p.append(n)
+#            print n
+
+print p
+print len(p)
+print phi
+'''
+
+
+
+for n in range(1,N): #530 corresponde a una orbita, aproximadamente
+    Shakuras.avanza_verlet(dt)
+    x[n]=Shakuras.y_actual[0]
+    y[n]=Shakuras.y_actual[1]
+    E[n]=Shakuras.energia_total()
+    r[n] = x[n]** 2 + y[n]** 2
+
+xm = []
+ym = []
+phi = []
+tm = []
+w = []
+
+
+for i in range(1,30):
+    T=int(len(x)/30) #periodo
+    ti = T*(i-1)
+    tf = T*(i)
+    a = x[ti: tf]
+    b = y[ti: tf]
+    c = r[ti: tf]
+    d = t[ti: tf]
+    n = np.where(c == c.max())
+    xm.append(a[n])
+    ym.append(b[n])
+    tm.append(d[n])
+    p = np.arctan(a[n]/ (float)(b[n]))
+    phi.append(p)
+    if i>=2:
+        dphi = phi[i-1] - phi[i-2]
+        dt = t[i] - t[i-1]
+        wi = dphi/ dt
+        w.append(wi)
+
+W = np.mean(w)
+print W
+
 
 
 ax1=fig.add_subplot(211)
